0dd11c3f6c8d3f73e594e63df92c0d94d209c639
Neha <53961604+aarneha@users.noreply.github.com>
2019-08-24 14:00:29 +0530

    iNVERSE OF A MATRIX
    INVERSE OF A MATRIX

---
+++ INVERSE OF A MATRIX.R
@@ -0,0 +1,23 @@
+myfunction <- function(){
+  x<-rnorm(100)
+  mean(x)
+}
+Second <- function(x){
+  x+rnorm(100)
+  x<-5
+  print(x)
+  y<-x+1
+  y
+}
+third <- function ()
+{
+  x<-1:20
+  x
+}
+myfirstvector <- Vector(character(5))
+x <- C(0.5,0.6)
+x
+
+
+m <- matrix(1:6,nrow=2,ncol=2)
+m
